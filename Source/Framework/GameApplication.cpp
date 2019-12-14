ad8621700dd2602807075fe435454ccbb6500f2b
deadbeef47 <46090066+deadbeef47@users.noreply.github.com>
2019-12-15 04:43:09 +0900

    フレームワークベースクラス(仮)を追加

---
+++ Source/Framework/GameApplication.cpp
@@ -0,0 +1,65 @@
+#include "GameApplication.h"
+#include "Utility\PotatoLog.h"
+
+#include <GL\glew.h>
+#include <GLFW\glfw3.h>
+//#include <glm\glm.hpp>
+#include "Framework\Window.h"
+
+namespace PotatoLib {
+
+	GameApplication::GameApplication() :
+		mMajorVersion(4), mMinorVersion(3) {
+	}
+
+	void GameApplication::Run() {
+
+		PotatoLog("glfw version : ", glfwGetVersionString());
+
+		glfwSetErrorCallback([](int aError, const char* aDescription) {
+			PotatoLog("Error Number: ", aError);
+			PotatoLog("Description: ", aDescription);
+		});
+
+		if (glfwInit() == GL_FALSE) {
+			PotatoLog("glfwInit error");
+			return;
+		}
+
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, mMajorVersion);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, mMinorVersion);
+		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+		atexit([]() { glfwTerminate(); });
+
+		Window lWindow;
+
+		glewExperimental = GL_TRUE;
+		GLenum lError = glewInit();
+		if (GLEW_OK != lError) {
+			PotatoLog("glewInit error : ", glewGetErrorString(lError));
+			return;
+		}
+
+		Initialize();
+
+		while (!(glfwWindowShouldClose(lWindow.mHandle) | glfwGetKey(lWindow.mHandle, GLFW_KEY_ESCAPE))) {
+
+			Update();
+
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+			glClearColor(0.0, 0.0, 0.0, 1.0);
+
+			Draw();
+
+			glfwSwapBuffers(lWindow.mHandle);
+			//glfwWaitEvents();
+			glfwPollEvents();
+		}
+
+		Finish();
+
+	}
+}
---
+++ Source/Framework/GameApplication.h
@@ -0,0 +1,23 @@
+#pragma once
+
+//#include "Source\Utility\Singleton.h"
+#include "Utility\Singleton.h"
+
+namespace PotatoLib {
+	class GameApplication {
+	public:
+		GameApplication();
+		void Run();
+
+	private:
+		virtual void Initialize() {}
+		virtual void Update() {}
+		virtual void Draw() {}
+		virtual void Finish() {}
+
+	protected:
+		int mMajorVersion, mMinorVersion;
+	private:
+	};
+}
+
