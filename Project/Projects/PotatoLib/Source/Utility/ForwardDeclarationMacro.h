4c1ef4c0399482a9b54cf42b85da232cefd0d0ea
deadbeef47 <kenoji47@gmail.com>
2023-07-22 18:00:04 +0900

    STL前方宣言用マクロ

---
+++ Project/Projects/PotatoLib/Source/Utility/ForwardDeclarationMacro.h
@@ -0,0 +1,18 @@
+﻿/*****************************************************************//**
+ * \file   ForwardDeclarationMacro.h
+ * \brief  前方宣言テンプレートマクロ
+ *********************************************************************/
+#pragma once
+
+namespace PotatoLib {
+	/**
+	 * std::string 前方宣言用マクロ.
+	 */
+#define STRING_FORWARD															\
+	namespace std {																\
+		template <class _Ty> class allocator;									\
+		template <class _Elem> struct char_traits;								\
+		template <class _Elem, class _Traits, class _Alloc> class basic_string; \
+		using string = basic_string<char, char_traits<char>, allocator<char>>;	\
+	}
+}
