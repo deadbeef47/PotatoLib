35684091ea9a548dd82cac647027fbb83752fb0d
deadbeef47 <kenoji47@gmail.com>
2023-07-26 11:09:38 +0900

    汎用パス編集クラス実装

---
+++ Project/Projects/PotatoLib/Source/Utility/PathUtility.h
@@ -0,0 +1,48 @@
+﻿/*****************************************************************//**
+ * \file   PathUtility.h
+ * \brief  パスパーサー
+ *********************************************************************/
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace PotatoLib {
+	/**
+	 * @class PathUtility
+	 * @brief パスパーサー.
+	 */
+	class PathUtility {
+	public:
+		/**
+		 * @brief パスからディレクトリ取得.
+		 * 
+		 * \param aPath パス
+		 * \return ディレクトリ名
+		 */
+		static std::string GetDirectory(const std::string& aPath);
+		/**
+		 * @brief パスからファイル名取得.
+		 * 
+		 * \param aPath パス
+		 * \return ファイル名
+		 */
+		static std::string GetFileName(const std::string& aPath);
+		/**
+		 * @brief パスから拡張子取得.
+		 * 
+		 * \param aPath パス
+		 * \return 拡張子
+		 */
+		static std::string GetFileExtension(const std::string& aPath);
+
+		/**
+		 * @brief フォルダ/ファイル単位で文字列を配列化する.
+		 * 
+		 * \param aPath パス
+		 * \return フォルダ/ファイル配列
+		 */
+		static std::vector<std::string> GetElementArray(const std::string& aPath);
+	private:
+	};
+}
