4449e79b37214cce308c63c4ca283290210b109b
deadbeef47 <kenoji47@gmail.com>
2023-07-26 11:05:57 +0900

    汎用文字列編集クラス実装

---
+++ Project/Projects/PotatoLib/Source/Utility/StringUtility.cpp
@@ -0,0 +1,15 @@
+﻿#include "StringUtility.h"
+#include <sstream>
+
+namespace PotatoLib {
+
+	std::vector<std::string> StringUtility::Split(const std::string& aText, char aDelim) {
+		std::vector<std::string> lRes = {};
+		std::stringstream lSs(aText);
+		std::string lItem = "";
+		while (std::getline(lSs, lItem, aDelim)) {
+			lRes.emplace_back(lItem);
+		}
+		return lRes;
+	}
+}
---
+++ Project/Projects/PotatoLib/Source/Utility/StringUtility.h
@@ -0,0 +1,28 @@
+﻿/*****************************************************************//**
+ * \file   StringUtility.h
+ * \brief  string関連汎用処理
+ *********************************************************************/
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace PotatoLib {
+	/**
+	 * @class StringUtility.
+	 * @brief string関連汎用処理
+	 */
+	class StringUtility {
+	public:
+		/**
+		 * @brief 文字列分割.
+		 * 
+		 * \param aText 分割対象文字列
+		 * \param aDelim デリミタ
+		 * \return  分割結果
+		 */
+		static std::vector<std::string> Split(const std::string& aText, char aDelim);
+
+	private:
+	};
+}
