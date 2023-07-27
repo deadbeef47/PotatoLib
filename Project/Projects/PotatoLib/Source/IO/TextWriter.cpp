58b067a83a2ed200fb18ba297cc2cfb494524000
deadbeef47 <kenoji47@gmail.com>
2023-07-28 07:51:11 +0900

    テキスト入出力クラス作成

---
+++ Project/Projects/PotatoLib/Source/IO/TextWriter.cpp
@@ -0,0 +1,46 @@
+﻿#include "TextWriter.h"
+
+#include <fstream>
+#include <direct.h>
+#include <errno.h>
+#include "Utility/PathUtility.h"
+
+namespace PotatoLib {
+
+	bool TextWriter::Save(const std::string& aPath, const std::string& aSaveText) {
+
+		if (!CreateDirectory(aPath)) return false;
+
+		std::ofstream lFile(aPath);
+		if (!lFile.is_open()) {
+			return false;
+		}
+
+		lFile << aSaveText;
+		return true;
+	}
+
+	bool TextWriter::CreateDirectory(const std::string& aPath) {
+		const auto lDirectory = PathUtility::GetDirectory(aPath);
+		const auto lFolderNames = PathUtility::GetElementArray(lDirectory);
+
+		bool lIsFailCreateDir = false;
+		std::string lCreatedFolder = "";
+		for (const auto& lFolderName : lFolderNames) {
+			lCreatedFolder += lFolderName;
+
+			errno = 0;
+			if (_mkdir(lCreatedFolder.c_str()) == -1) continue;
+
+			// 指定パスは存在しない
+			if (errno == ENOENT) {
+				lIsFailCreateDir = true;
+				break;
+			}
+			lCreatedFolder += "/";
+		}
+		errno = 0;
+
+		return !lIsFailCreateDir;
+	}
+}
---
+++ Project/Projects/PotatoLib/Source/IO/TextWriter.h
@@ -0,0 +1,38 @@
+﻿/*****************************************************************//**
+ * \file   TextWriter.h
+ * \brief  テキスト出力
+ *********************************************************************/
+#pragma once
+
+#include "Utility/ForwardDeclarationMacro.h"
+
+STRING_FORWARD
+
+namespace PotatoLib {
+	/**
+	 * @class TextReader.
+	 * @brief テキスト入力
+	 */
+	class TextWriter {
+	public:
+		/**
+		 * @brief ファイルセーブ.
+		 *
+		 * \param aPath セーブ用 パス/ファイル名
+		 * \param aSaveText セーブするテキスト
+		 * \return 成功/失敗
+		 */
+		static bool Save(const std::string& aPath, const std::string& aSaveText);
+
+	private:
+		/**
+		 * @brief フォルダ作成.
+		 * 
+		 * \param aPath 指定のパス
+		 * \return フォルダ作成に成功したか
+		 */
+		static bool CreateDirectory(const std::string& aPath);
+
+	private:
+	};
+}
